0e52a0ceea66b502f3016649583a59ab9ed28100
mohcine elbah <mohcine.elbah@gmail.com>
2023-10-12 12:40:23 +0200

    inscription form created

---
+++ Board/src/Register/Register.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import './styles.css';  // Import the CSS file
+
+const Register: React.FC = () => {
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value);
+    };
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log('Form submitted:', { name, email, password });
+    };
+
+    return (
+        <form className="registration-form" onSubmit={handleSubmit}>
+            <label>
+                Name:
+                <input type="text" value={name} onChange={handleNameChange} />
+            </label>
+            <br />
+            <label>
+                Email:
+                <input type="email" value={email} onChange={handleEmailChange} />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" value={password} onChange={handlePasswordChange} />
+            </label>
+            <br />
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default Register;
