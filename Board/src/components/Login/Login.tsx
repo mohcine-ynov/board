48c077082303e2fb5f3620354254396fd4b10cb0
mohcine elbah <mohcine.elbah@gmail.com>
2023-10-12 16:55:35 +0200

    linked to api

--- Board/src/components/Login/Login.tsx
+++ Board/src/components/Login/Login.tsx
@@ -1,9 +1,39 @@
 import { useState } from 'react';
 import './styles.css';  // Import the CSS file
+import axios from 'axios';
+
+async function authenticateUser(username, password) {
+    let users;
+    let user;
+
+    try {
+        console.log('Fetching data...');
+        const response = await axios.get('http://10.31.34.17:1337/api/utilisateurs');
+        // Process the response.data or perform actions after the GET request is complete
+        console.log('Data:', response.data);
+        users = response.data.data;
+        user = users.find((u) => u.attributes.email === username);
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+    }
+
+    console.log(user);
+
+    if (!user) {
+        return { success: false, message: 'User not found' };
+    }
+
+    if (user.attributes.password !== password) {
+        return { success: false, message: 'Incorrect password' };
+    }
+
+    return { success: true, user };
+}
 
 const Login: React.FC = () => {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [error, setError] = useState<string>('');
 
     const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value);
@@ -13,25 +43,45 @@ const Login: React.FC = () => {
         setPassword(e.target.value);
     };
 
-    const handleSubmit = (e: React.FormEvent) => {
+    async function handleSubmit(e: React.FormEvent) {
+
         e.preventDefault();
-        console.log('Form submitted:', { email, password });
+
+        try {
+            const authenticationResult = await authenticateUser(email, password);
+
+            if (authenticationResult.success) {
+                // Successful login - handle login logic (e.g., store authentication token)
+                console.log('Login successful:', authenticationResult.user);
+                setError('Success!');
+            } else {
+                // Failed login
+                setError(authenticationResult.message);
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
     };
 
+
     return (
-        <form className="registration-form" onSubmit={handleSubmit}>
-            <label>
-                Email:
-                <input type="email" value={email} onChange={handleEmailChange} />
-            </label>
-            <br />
-            <label>
-                Password:
-                <input type="password" value={password} onChange={handlePasswordChange} />
-            </label>
-            <br />
-            <button type="submit">Submit</button>
-        </form>
+        <div>
+            <form className="registration-form" onSubmit={handleSubmit}>
+                <label>
+                    Email:
+                    <input type="email" value={email} onChange={handleEmailChange} />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input type="password" value={password} onChange={handlePasswordChange} />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+            {error && <div style={{ color: 'red' }}>{error}</div>}
+        </div>
+
     );
 };
 
